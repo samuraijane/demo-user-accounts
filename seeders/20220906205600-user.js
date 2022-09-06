62b13b87c43c857448aa9485b0c353766a3fbc5b
Matthew Day <matt@matthewday.net>
2022-09-06 17:03:49 -0400

    add initial data to user seed file

--- seeders/20220906205600-user.js
+++ seeders/20220906205600-user.js
@@ -2,23 +2,20 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Andrea',
+        lastName: 'Anderson',
+        email: 'andrea@someemail.com',
+        password: 'somePlainTextPasswordForNow',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
