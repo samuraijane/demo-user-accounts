99747e3f228a4ccbcd4674b28403769bebc17c99
Matthew Day <matt@matthewday.net>
2022-09-06 17:23:43 -0400

    add POST for /users (creates a new user)

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { User } = require('./models');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.json({location: '/'})
 });
@@ -11,6 +13,22 @@ server.get('/', (req, res) => {
 server.get('/users', async (req, res) => {
   const users = await User.findAll();
   res.json(users);
+});
+
+server.post('/users', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    email,
+    password
+  });
+
+  res.json({
+    id: newUser.id,
+    message: 'new user created',
+  })
+
 });
 
 server.listen(8080, () => {
