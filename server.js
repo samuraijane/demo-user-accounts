31f5a0ac48d9c526df4d1afcb25351635f3cab8d
Matthew Day <matt@matthewday.net>
2022-09-06 17:17:23 -0400

    add GET for /users (returns all users)

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { User } = require('./models');
 
 const server = express();
 
 server.get('/', (req, res) => {
   res.json({location: '/'})
+});
+
+// demo only, do not allow just anyone to access all the users in your database
+server.get('/users', async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
 });
 
 server.listen(8080, () => {
